93363d30e7cbb0da92556e68a3f7502bbfbdf349
Rene <rrr333nnn333@gmail.com>
2026-04-06 01:09:34 -0400

    add: disable other Zygisk impl

--- module/src/customize.sh
+++ module/src/customize.sh
@@ -1,3 +1,12 @@
+# Disable Other Zygisk
+modules="
+zygisksu
+"
+for i in ${modules}; do
+	[[ -e "/data/adb/modules/${i}" ]] && touch "/data/adb/modules/${i}/disable"
+done
+[[ -e "/data/adb/modules/brezygisk" ]] && touch "/data/adb/modules/brezygisk/remove"
+
 # shellcheck disable=SC2034
 SKIPUNZIP=1
 
