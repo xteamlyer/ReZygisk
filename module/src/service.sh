1a3d5887d53f53340cc28694881a3ee03b658894
Rene <rrr333nnn333@gmail.com>
2026-04-22 20:35:08 -0400

    drop: Magisk support

--- module/src/service.sh
+++ module/src/service.sh
@@ -6,25 +6,4 @@ DEBUG=@DEBUG@
 
 MODDIR=${0%/*}
 
-if [ "$ZYGISK_ENABLED" ]; then
-  sed -i "s|^description=|description=[❌ Disable Magisk's built-in Zygisk] |" "$MODDIR/module.prop"
-
-  exit 0
-fi
-
-cd "$MODDIR"
-
-if [ "$(which magisk)" ]; then
-  for file in ../*; do
-    if [ -d "$file" ] && [ -d "$file/zygisk" ] && ! [ -f "$file/disable" ]; then
-      if [ -f "$file/service.sh" ]; then
-        cd "$file"
-        log -p i -t "zygisk-sh" "Manually trigger service.sh for $file"
-        sh "$(realpath ./service.sh)" &
-        cd "$MODDIR"
-      fi
-    fi
-  done
-fi
-
 exit 0
