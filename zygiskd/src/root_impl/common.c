c9602216597c561efa485090b48d3cb7b8a2afca
Rene <rrr333nnn333@gmail.com>
2026-04-22 21:04:05 -0400

    drop: APatch support

--- zygiskd/src/root_impl/common.c
+++ zygiskd/src/root_impl/common.c
@@ -3,7 +3,6 @@
 #include "common.h"
 
 #include "../utils.h"
-#include "apatch.h"
 #include "kernelsu.h"
 
 static struct root_impl impl;
@@ -12,26 +11,16 @@ void root_impls_setup(void) {
   struct root_impl_state state_ksu;
   ksu_get_existence(&state_ksu);
 
-  struct root_impl_state state_apatch;
-  apatch_get_existence(&state_apatch);
-
   /* INFO: Check if it's only one supported, if not, it's multile and that's bad.
             Remember that true here is equal to the integer 1. */
   if (state_ksu.state == Supported) {
     impl.impl = KernelSU;
     impl.variant = state_ksu.variant;
-  } else if (state_apatch.state == Supported) {
-    impl.impl = APatch;
   }
 
   switch (impl.impl) {
     case KernelSU: {
       LOGI("KernelSU root implementation found.\n");
-
-      break;
-    }
-    case APatch: {
-      LOGI("APatch root implementation found.\n");
 
       break;
     }
@@ -47,19 +36,13 @@ bool uid_granted_root(uid_t uid) {
     case KernelSU: {
       return ksu_uid_granted_root(uid);
     }
-    case APatch: {
-      return apatch_uid_granted_root(uid);
-    }
   }
 }
 
-bool uid_should_umount(uid_t uid, const char *const process) {
+bool uid_should_umount(uid_t uid) {
   switch (impl.impl) {
     case KernelSU: {
       return ksu_uid_should_umount(uid);
-    }
-    case APatch: {
-      return apatch_uid_should_umount(uid, process);
     }
   }
 }
@@ -68,9 +51,6 @@ bool uid_is_manager(uid_t uid) {
   switch (impl.impl) {
     case KernelSU: {
       return ksu_uid_is_manager(uid);
-    }
-    case APatch: {
-      return apatch_uid_is_manager(uid);
     }
   }
 }
