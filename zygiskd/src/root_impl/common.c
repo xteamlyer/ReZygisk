30c2b7ea2012c2ea553a5baf3fdd57f952fc2793
Rene <rrr333nnn333@gmail.com>
2026-04-22 20:35:08 -0400

    drop: Magisk support

--- zygiskd/src/root_impl/common.c
+++ zygiskd/src/root_impl/common.c
@@ -5,7 +5,6 @@
 #include "../utils.h"
 #include "apatch.h"
 #include "kernelsu.h"
-#include "magisk.h"
 
 static struct root_impl impl;
 
@@ -16,36 +15,16 @@ void root_impls_setup(void) {
   struct root_impl_state state_apatch;
   apatch_get_existence(&state_apatch);
 
-  struct root_impl_state state_magisk;
-  magisk_get_existence(&state_magisk);
-
   /* INFO: Check if it's only one supported, if not, it's multile and that's bad.
             Remember that true here is equal to the integer 1. */
-  if ((state_ksu.state == Supported ? 1 : 0) + (state_apatch.state == Supported ? 1 : 0) + (state_magisk.state == Supported ? 1 : 0) >= 2) {
-    impl.impl = Multiple;
-  } else if (state_ksu.state == Supported) {
+  if (state_ksu.state == Supported) {
     impl.impl = KernelSU;
     impl.variant = state_ksu.variant;
   } else if (state_apatch.state == Supported) {
     impl.impl = APatch;
-  } else if (state_magisk.state == Supported) {
-    impl.impl = Magisk;
-    impl.variant = state_magisk.variant;
-  } else {
-    impl.impl = None;
   }
 
   switch (impl.impl) {
-    case None: {
-      LOGI("No root implementation found.\n");
-
-      break;
-    }
-    case Multiple: {
-      LOGI("Multiple root implementations found.\n");
-
-      break;
-    }
     case KernelSU: {
       LOGI("KernelSU root implementation found.\n");
 
@@ -53,11 +32,6 @@ void root_impls_setup(void) {
     }
     case APatch: {
       LOGI("APatch root implementation found.\n");
-
-      break;
-    }
-    case Magisk: {
-      LOGI("Magisk root implementation found.\n");
 
       break;
     }
@@ -76,12 +50,6 @@ bool uid_granted_root(uid_t uid) {
     case APatch: {
       return apatch_uid_granted_root(uid);
     }
-    case Magisk: {
-      return magisk_uid_granted_root(uid);
-    }
-    default: {
-      return false;
-    }
   }
 }
 
@@ -93,12 +61,6 @@ bool uid_should_umount(uid_t uid, const char *const process) {
     case APatch: {
       return apatch_uid_should_umount(uid, process);
     }
-    case Magisk: {
-      return magisk_uid_should_umount(process);
-    }
-    default: {
-      return false;
-    }
   }
 }
 
@@ -109,12 +71,6 @@ bool uid_is_manager(uid_t uid) {
     }
     case APatch: {
       return apatch_uid_is_manager(uid);
-    }
-    case Magisk: {
-      return magisk_uid_is_manager(uid);
-    }
-    default: {
-      return false;
     }
   }
 }
